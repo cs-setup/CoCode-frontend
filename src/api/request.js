4cebc1974bb8c3ead6a6aeef35f87a472314583c
YuZzzzzzzze <10857440+yuzzzzzzzze@user.noreply.gitee.com>
2023-03-22 15:45:23 +0800

    feat: packaging axios

---
+++ src/api/request.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import {
+    message
+} from 'antd';
+
+const instance = axios.create({
+    baseURL: process.env.NODE_ENV === 'production' ? 'http://production-url.com/' : 'https://mock.apifox.cn/m1/2452451-0-default/',
+    timeout: 1000,
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
+
+instance.interceptors.request.use(function (config) {
+    if (!config.headers.notoken) {
+        const token = localStorage.getItem('token');
+        config.headers.Authorization = `${token}`
+    }
+    // 在发送请求之前做些什么
+    return config;
+}, function (error) {
+    // 对请求错误做些什么
+    message.error(error.message);
+    return Promise.reject(error);
+});
+
+instance.interceptors.response.use(function (response) {
+    // 对响应数据做点什么
+    return response;
+}, function (error) {
+    // 对响应错误做点什么
+    message.error(error.message);
+    return Promise.reject(error);
+});
+
+export default instance;
---
+++ src/api/user/index.js
@@ -0,0 +1,29 @@
+import request from "../request"
+export const register = async (params) => {
+    const result = await request.post('user/register', params, {
+        headers: {
+            'notoken': true
+        }
+    })
+    return result.data
+}
+
+export const login = async (params) => {
+    const result = await request.post('user/login', params, {
+        headers: {
+            'notoken': true
+        }
+    })
+    return result
+}
+
+export const verify  = async (params) => {
+    console.log(params);
+    const result = await request.post('user/message',params,{
+        headers: {
+            'notoken': true
+        }
+    })
+    console.log(result);
+    return result
+}
