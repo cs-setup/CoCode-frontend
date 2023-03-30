7a8dfd484d3be94cd87424aafa8d3c4f53f43107
YuZzzzzzzze <10857440+yuzzzzzzzze@user.noreply.gitee.com>
2023-03-30 17:34:22 +0800

    feat: two column layout

---
+++ src/components/Layout/TwoColumn/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Row, Col } from "antd";
+
+const TwoColumn = (props) => {
+  return (
+    <Row justify="center" gutter={12}>
+      <Col xs={23} sm={23} md={16} lg={16} xl={16} xxl={12}>
+        {props.left}
+      </Col>
+      <Col xs={0} sm={0} md={6} lg={6} xl={6} xxl={5}>
+        {props.right}
+      </Col>
+    </Row>
+  );
+};
+
+export default TwoColumn;
