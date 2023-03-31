95436db75fdda04462fa88c9c0a92be9ebf09a30
YuZzzzzzzze <10857440+yuzzzzzzzze@user.noreply.gitee.com>
2023-03-31 14:54:20 +0800

    fix: show publish feed

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Card, Space } from "antd";
 import { HomeProvider } from "../../contexts/HomeContext";
+import { LoginContext } from "../../contexts/LoginContext";
 import TwoColumn from "../../components/Layout/TwoColumn";
 import FeedList from "../../components/List/FeedList";
 import FeedEdit from "../../components/FeedEdit";
 
 const LeftColumn = () => {
+  const { isLoggedIn } = useContext(LoginContext);
   return (
     <>
       <Space direction="vertical" size="middle" style={{ display: "flex" }}>
-        <Card>
-          <FeedEdit />
-        </Card>
+        {isLoggedIn && (
+          <Card>
+            <FeedEdit />
+          </Card>
+        )}
         <Card title={<div>沸点列表</div>}>
           <FeedList />
         </Card>
