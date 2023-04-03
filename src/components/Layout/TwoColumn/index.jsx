734566e6ec6f71c9bed43ce20f257d3b38a56859
YuZzzzzzzze <10857440+yuzzzzzzzze@user.noreply.gitee.com>
2023-04-03 13:12:18 +0800

    fix: change two column layout

--- src/components/Layout/TwoColumn/index.jsx
+++ src/components/Layout/TwoColumn/index.jsx
@@ -4,7 +4,7 @@ import { Row, Col } from "antd";
 const TwoColumn = (props) => {
   return (
     <Row justify="center" gutter={12} style={{width: "100%"}}>
-      <Col xs={23} sm={23} md={16} lg={16} xl={16} xxl={12}>
+      <Col xs={23} sm={23} md={16} lg={14} xl={12} xxl={10}>
         {props.left}
       </Col>
       <Col xs={0} sm={0} md={6} lg={6} xl={6} xxl={5}>
