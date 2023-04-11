f936e516007331225cd6a4b03f07c09e6d308721
YuZzzzzzzze <10857440+yuzzzzzzzze@user.noreply.gitee.com>
2023-04-11 15:30:47 +0800

    feat: CenterItem

---
+++ src/components/CenterItem/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Row, Col } from 'antd'
+
+const CenterItem = ({children}) => {
+  console.log(children);
+  return (
+    <Row justify="center">
+        <Col span={24} style={{textAlign: "center"}}>{children}</Col>
+    </Row>
+  )
+}
+
+export default CenterItem
