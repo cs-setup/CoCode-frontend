1b783c4517b33f9b40deb3595a5c093be018e62b
YuZzzzzzzze <10857440+yuzzzzzzzze@user.noreply.gitee.com>
2023-04-22 22:59:29 +0800

    feat: noheader

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -4,12 +4,14 @@ import { useLocation } from "react-router-dom";
 import Header from "../Header";
 import Footer from "../Footer";
 
+const noHeader = ["/editor", "/chat/message"];
+
 export default function Layout(props) {
   const location = useLocation();
   return (
     <>
       <Space direction="vertical" style={{ display: "flex" }} size="middle">
-        {location.pathname !== "/editor" && <Header />}
+        {noHeader.findIndex((path)=> path == location.pathname) === -1 && <Header />}
         {props.children}
         {/* <Footer /> */}
       </Space>
