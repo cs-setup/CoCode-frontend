5e755928e22ee9a9a80d3ba971b383b7e9674361
YuZzzze <1162367836@qq.com>
2023-05-24 23:25:34 +0800

    fix: 525 raffle

--- src/pages/Raffle/index.jsx
+++ src/pages/Raffle/index.jsx
@@ -2,40 +2,50 @@ import React, { useState } from "react";
 import { InputNumber, Button, Row, Col, Space, Card, Spin } from "antd";
 import { useEffect } from "react";
 
-let usedNumbers = []
+let usedNumbers = [];
 
 const Raffle = () => {
   const [minValue, setMinValue] = useState(0);
   const [maxValue, setMaxValue] = useState(10);
+  const [minValue2, setMinValue2] = useState(0);
+  const [maxValue2, setMaxValue2] = useState(10);
   const [quantity, setQuantity] = useState(1);
   const [loading, setLoading] = useState(false);
   const [randomNumbers, setRandomNumbers] = useState([]);
 
   const handleMinValueChange = (value) => {
     setMinValue(value);
-    usedNumbers=[]
+    usedNumbers = [];
   };
 
   const handleMaxValueChange = (value) => {
     setMaxValue(value);
-    usedNumbers=[]
+    usedNumbers = [];
   };
 
   const handleQuantityChange = (value) => {
     setQuantity(value);
-    usedNumbers=[]
+    usedNumbers = [];
   };
 
   const handleGenerateClick = () => {
     setLoading(true);
     const numbers = [];
     while (numbers.length < quantity) {
-      const randomNumber =
-        Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
-      if (!numbers.includes(randomNumber) && !usedNumbers.includes(randomNumber)) {
-        numbers.push(randomNumber);
-        usedNumbers.push(randomNumber)
-      }
+      const randomNumber = [
+        Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue2,
+        Math.floor(Math.random() * (maxValue2 - minValue2 + 1)) + minValue2,
+      ];
+      console.log(randomNumber);
+      // Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+      //   if (
+      //     !numbers.includes(randomNumber)
+      //     // !usedNumbers.includes(randomNumber)
+      //   ) {
+      //     numbers.push(randomNumber);
+      //     // usedNumbers.push(randomNumber);
+      //   }
+      numbers.push(randomNumber);
     }
     setRandomNumbers(numbers);
   };
@@ -52,7 +62,7 @@ const Raffle = () => {
     <div
       style={{
         backgroundColor: "#fff",
-        backgroundImage: `url(https://remote-obj-1313529792.cos.ap-guangzhou.myqcloud.com/cocode/6e5eeeb6-fd57-42aa-8052-6916646d6c2a.jpg)`,
+        backgroundImage: `url(https://www.helloimg.com/images/2023/05/24/oJtP6P.jpg)`,
         backgroundRepeat: "no-repeat",
         backgroundPosition: "center",
       }}
@@ -61,34 +71,64 @@ const Raffle = () => {
         <Col>
           <Row justify="center">
             <Col>
-              <div style={{ padding: 40, fontSize: 48, fontWeight: "bold" }}>
-                木心十周年献礼抽奖
+              <div style={{ padding: 40, fontSize: 48, fontWeight: "bold", color: '#fff' }}>
+              525音乐故事会暖场抽奖
               </div>
             </Col>
           </Row>
           <Space direction="vertical" size="large" style={{ display: "flex" }}>
             <Card style={{ opacity: 0.99 }} size="small">
-              <Row justify="center" gutter={16}>
+              <Row justify="center" align="middle" gutter={[16, 16]}>
                 <Col>
-                  <Space>
-                    <InputNumber
-                      value={minValue}
-                      onChange={handleMinValueChange}
-                      min={0}
-                      max={maxValue}
-                    />
-                    <span>~</span>
-                  </Space>
+                  <Row>
+                    排：
+                    <Col>
+                      <Space>
+                        <InputNumber
+                          value={minValue}
+                          onChange={handleMinValueChange}
+                          min={0}
+                          max={maxValue}
+                        />
+                        <span>~</span>
+                      </Space>
+                    </Col>
+                    <Col>
+                      <Space>
+                        <InputNumber
+                          value={maxValue}
+                          onChange={handleMaxValueChange}
+                          min={minValue}
+                        />
+                      </Space>
+                    </Col>
+                  </Row>
+                  <Row style={{ marginTop: 12 }}>
+                    号：
+                    <Col>
+                      <Space>
+                        <InputNumber
+                          value={minValue2}
+                          onChange={handleMinValueChange}
+                          min={0}
+                          max={maxValue2}
+                        />
+                        <span>~</span>
+                      </Space>
+                    </Col>
+                    <Col>
+                      <Space>
+                        <InputNumber
+                          value={maxValue}
+                          onChange={handleMaxValueChange}
+                          min={minValue}
+                        />
+                      </Space>
+                    </Col>
+                  </Row>
                 </Col>
                 <Col>
-                  <Space>
-                    <InputNumber
-                      value={maxValue}
-                      onChange={handleMaxValueChange}
-                      min={minValue}
-                    />
-                    <span>x</span>
-                  </Space>
+                  <span>x</span>
                 </Col>
                 <Col>
                   <InputNumber
@@ -97,6 +137,8 @@ const Raffle = () => {
                     min={1}
                   />
                 </Col>
+              </Row>
+              <Row justify="center" style={{ marginTop: 16 }}>
                 <Col>
                   <Button onClick={handleGenerateClick} type="primary ">
                     开始抽奖
@@ -116,7 +158,7 @@ const Raffle = () => {
                 size="small"
               >
                 <Spin spinning={loading} size="large" tip="抽取号码中...">
-                  {randomNumbers.map((number, index) => (
+                  {randomNumbers.map((numbers, index) => (
                     <Row
                       key={index}
                       justify="center"
@@ -130,7 +172,7 @@ const Raffle = () => {
                             color: "#696969",
                           }}
                         >
-                          {number}
+                          {numbers[0]} 排 {numbers[1]} 号
                         </div>
                       </Col>
                     </Row>
